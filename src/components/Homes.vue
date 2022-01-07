ca92247d6258dc22bd115a401b17677aa4565ec4
Julio Velazco <47427287+Velazco1997@users.noreply.github.com>
2022-01-07 01:19:32 -0500

    a dormir

--- src/components/Homes.vue
+++ src/components/Homes.vue
@@ -2,18 +2,21 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h1>Menú</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+
+    <h3 id='entrante'>Entrantes</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3 id='platos-fuertes'>Platos Fuertes</h3>
+    <h4 id='pollo'>Pollo</h4>
+    <h4 id='cerdo'>Cerdo</h4>
+    <h4 id='camaron'>Camarón</h4>
+    <h4 id='pescado'>Pescado</h4>
+    <h4 id='otros-platos'>Otros Platos</h4>
+    <h4 id='pastas'>Pastas</h4>
+    <h4 id='entrepanes'>Entrepanes</h4>
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
@@ -21,7 +24,7 @@
       <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
     </ul>
-    <h3 id="entrante">Ecosystem</h3>
+    <h3>Bebidas</h3>
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
